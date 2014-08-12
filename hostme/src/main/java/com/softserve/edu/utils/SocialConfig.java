5ebd1e58201c1879aedf24def6ad64d5611dc58b
YuriiYurii <famaous@ukr.net>
2014-08-12 12:35:32 +0300

    utils for facebook service

---
+++ hostme/src/main/java/com/softserve/edu/utils/SocialConfig.java
@@ -0,0 +1,36 @@
+package com.softserve.edu.utils;
+
+import org.springframework.core.env.Environment;
+import org.springframework.social.UserIdSource;
+import org.springframework.social.config.annotation.ConnectionFactoryConfigurer;
+import org.springframework.social.config.annotation.SocialConfigurer;
+import org.springframework.social.connect.ConnectionFactoryLocator;
+import org.springframework.social.connect.UsersConnectionRepository;
+import org.springframework.social.facebook.connect.FacebookConnectionFactory;
+
+public class SocialConfig implements SocialConfigurer {
+
+	@Override
+	public void addConnectionFactories(ConnectionFactoryConfigurer cfConfig,
+			Environment env) {
+		cfConfig.addConnectionFactory(new FacebookConnectionFactory(env
+				.getProperty("facebook.clientId"), env
+				.getProperty("facebook.clientSecret")));
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public UserIdSource getUserIdSource() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public UsersConnectionRepository getUsersConnectionRepository(
+			ConnectionFactoryLocator arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
