ee1cf3cceec215bd706b1af2389a1f84e5b3dff0
npu3pak0406 <static-theonly@rambler.ru>
2014-08-13 16:39:17 +0300

    connection profile.jsp with DB

--- hostme/src/main/java/com/softserve/edu/web/ProfileController.java
+++ hostme/src/main/java/com/softserve/edu/web/ProfileController.java
@@ -1,15 +1,43 @@
 package com.softserve.edu.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.softserve.edu.entity.User;
+import com.softserve.edu.service.ProfileService;
+import com.softserve.edu.service.UserService;
+
 @Controller
 public class ProfileController {
+	@Autowired
+	private ProfileService profileService;
+	
+	@Autowired
+	private UserService userService;
 
-    @RequestMapping(value = "/profile", method = RequestMethod.GET)
-    public String profileShow(Model model) {
-            return "profile";
-    }
+	@RequestMapping(value = "/profile", method = RequestMethod.GET)
+	public String profileShow(Model model) {
+
+		Authentication authentication = SecurityContextHolder.getContext()
+				.getAuthentication();
+		String currentPrincipalName = authentication.getName();
+
+		User user = profileService.getUserByLogin(currentPrincipalName);
+
+		int age = profileService.calcAge(user);
+		String birth = profileService.receiveBirthday(user);
+
+		userService.initilizeUserLanguages(user);
+
+		model.addAttribute("age", age);
+		model.addAttribute("birth", birth);
+		model.addAttribute("user", user);
+
+		return "profile";
+	}
 }
