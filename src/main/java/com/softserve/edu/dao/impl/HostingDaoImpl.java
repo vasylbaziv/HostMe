ce3be7c4428357635ff26182c26cdbf48b3ee635
louriel <viktoriiaromaniv@gmail.com>
2014-09-11 01:00:57 +0300

    Update Search with pagination.

--- src/main/java/com/softserve/edu/dao/impl/HostingDaoImpl.java
+++ src/main/java/com/softserve/edu/dao/impl/HostingDaoImpl.java
@@ -22,31 +22,60 @@ public class HostingDaoImpl extends AbstractGenericDao<Hosting, Integer> impleme
 
     @Autowired
     private SessionFactory sessionFactory;
+    
+    private static final Integer ROWS = 3;    
 
-    public List<Hosting> listAllHostels() {
+    /*
+    Get list of the hostels that are required criterias
+     */
+    public List<Hosting> getList(List<Search> listSearch, Integer page) {
         Session session = sessionFactory.getCurrentSession();
-        Criteria cr = session.createCriteria(Hosting.class, "hostel");
-
-        //Join
+        Criteria cr = session.createCriteria(Hosting.class, "hosting").setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
         cr.setFetchMode("hosting.user", FetchMode.JOIN);
+        cr.setFetchMode("hosting.request", FetchMode.JOIN);
+
+        cr.setMaxResults(ROWS);
+        cr.setFirstResult(page * ROWS);
+
+        cr = getCriteriasResult(cr, listSearch);
         return cr.list();
     }
 
-
-    public List<Hosting> getList(Search... parameters) {
+    /*
+    Get count of pages
+     */
+    public Integer getCountOfPages(List<Search> listSearch) {
         Session session = sessionFactory.getCurrentSession();
-        Criteria cr = session.createCriteria(Hosting.class, "hosting");
+        Criteria cr = session.createCriteria(Hosting.class, "hosting").setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
+        cr = getCriteriasResult(cr, listSearch);
+        if (cr.list().size() % ROWS == 0)
+            return (cr.list().size() / ROWS);
+        else
+            return (cr.list().size() / ROWS + 1);
+    }
 
-        //Join
-        cr.setFetchMode("hosting.user", FetchMode.JOIN);
-        
+    public Gender parsingGender(String gender) {
+        for (Gender parameterGender : Gender.values()) {
+            if (gender.equalsIgnoreCase(parameterGender.toString())) {
+                return parameterGender;
+            }
+        }
+        return Gender.UNSPECIFIED;
+    }
+
+
+    /*
+    Return the criteria result for hostels
+     */
+    public Criteria getCriteriasResult(Criteria cr, List<Search> listSearch) {
+
         String min = new String();
         String valueMin = new String();
         String max = new String();
         String valueMax = new String();
 
         //add criterias
-        for (Search parameter : parameters) {
+        for (Search parameter : listSearch) {
             if ((parameter.getName().equals("country") || parameter.getName().equals("region")) && !parameter.getValue().isEmpty() && !parameter.getValue().equals(" ")) {
                 cr.add(Restrictions.eq(parameter.getName(), parameter.getValue()));
             }
@@ -54,7 +83,7 @@ public class HostingDaoImpl extends AbstractGenericDao<Hosting, Integer> impleme
                     || parameter.getName().equals("smoking")) && !parameter.getValue().isEmpty() && !parameter.getValue().equals("false")) {
                 cr.add(Restrictions.eq(parameter.getName(), Boolean.parseBoolean(parameter.getValue())));
             }
-            if (parameter.getName().equals("minNumberOfGuests") ||parameter.getName().equals("maxNumberOfGuests")) {
+            if (parameter.getName().equals("minNumberOfGuests") || parameter.getName().equals("maxNumberOfGuests")) {
                 if (parameter.getName().equals("minNumberOfGuests") && !parameter.getValue().isEmpty()) {
                     min = parameter.getName();
                     valueMin = parameter.getValue();
@@ -71,18 +100,18 @@ public class HostingDaoImpl extends AbstractGenericDao<Hosting, Integer> impleme
             if (parameter.getName().equals("gender")) {
                 cr.add(Restrictions.eq(parameter.getName(), parsingGender(parameter.getValue())));
             }
+//            if (parameter.getName().equals("begin_date") && !parameter.getValue().isEmpty()) {
+//                Calendar cal = Calendar.getInstance();
+//                cal.setTimeInMillis(Integer.parseInt(parameter.getValue()));
+//                cr.add(Restrictions.gt(parameter.getName(), cal));
+//            }
+//            if (parameter.getName().equals("end_date") && !parameter.getValue().isEmpty()) {
+//                Calendar cal = Calendar.getInstance();
+//                cal.setTimeInMillis(Integer.parseInt(parameter.getValue()));
+//                cr.add(Restrictions.lt(parameter.getName(), cal));
+//            }
         }
-        
-        return cr.list();
-    }
-    
-    public Gender parsingGender(String gender) {
-        for (Gender parameterGender : Gender.values()) {
-            if (gender.equalsIgnoreCase(parameterGender.toString())) {
-                return parameterGender;
-            }
-        }
-        return Gender.UNSPECIFIED;
+        return cr;
     }
 }
 
