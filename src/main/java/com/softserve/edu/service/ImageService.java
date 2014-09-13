056ac4f464f97b20edc86514ccd12fdbccb8317b
EugeneSmoliy <oceolathelead@gmail.com>
2014-09-13 22:21:37 +0300

    bug fix

--- src/main/java/com/softserve/edu/service/ImageService.java
+++ src/main/java/com/softserve/edu/service/ImageService.java
@@ -5,10 +5,8 @@ import java.util.List;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.softserve.edu.entity.Hosting;
-import com.softserve.edu.entity.Image;
 
 public interface ImageService {
-	public static final String filePath="D:\\HostMeImages\\";
 	
 	void addImages(MultipartFile[] files, Hosting hosting);
 	
