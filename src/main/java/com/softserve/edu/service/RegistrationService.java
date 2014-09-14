cbd5794d131507f6c285d4c9375242582f8eda01
louriel <viktoriiaromaniv@gmail.com>
2014-09-15 01:18:42 +0300

    Add sending notification mail.

--- src/main/java/com/softserve/edu/service/RegistrationService.java
+++ src/main/java/com/softserve/edu/service/RegistrationService.java
@@ -1,14 +1,17 @@
 package com.softserve.edu.service;
 
 import com.softserve.edu.entity.User;
+import com.softserve.edu.entity.UserState;
 
 public interface RegistrationService {
 	public void register(User user);
 
-	public void register(User user, String date, String gender);
+	public void register(User user, String date, String gender, UserState userState);
 
 	public User getUserByEmail(String email);
 
 	public User getUserByLogin(String login);
+	
+	public void activateAccount(Integer userId);
 
 }
