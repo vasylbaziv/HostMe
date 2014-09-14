8704b09f041f57d9937e2b75e3b536795241f776
louriel <viktoriiaromaniv@gmail.com>
2014-09-15 01:26:14 +0300

    Add sending notification mail.

--- src/main/java/com/softserve/edu/web/RegistrationController.java
+++ src/main/java/com/softserve/edu/web/RegistrationController.java
@@ -1,5 +1,7 @@
 package com.softserve.edu.web;
 
+import com.softserve.edu.entity.UserState;
+import com.softserve.edu.service.implementation.RegistrationSendMailImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,24 +12,40 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.softserve.edu.entity.User;
 import com.softserve.edu.service.RegistrationService;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RegistrationController {
-	@Autowired
-	private RegistrationService registrationService;
+    @Autowired
+    private RegistrationService registrationService;
 
-	@RequestMapping(value = "/registration", method = RequestMethod.GET)
-	public String registrationShow(Model model) {
-		return "redirect:/index";
-	}
+    @Autowired
+    private RegistrationSendMailImpl registrationSendMail;
 
-	@RequestMapping(value = "/registration", method = RequestMethod.POST)
-	public String addContact(@ModelAttribute("user") User user,
-			BindingResult bindingResult) {
-		String birthday = (String) bindingResult.getFieldValue("birthday");
-		String gender = (String) bindingResult.getFieldValue("gender");
+    @RequestMapping(value = "/registration", method = RequestMethod.GET)
+    public String registrationShow(Model model) {
+        return "redirect:/index";
+    }
 
-		registrationService.register(user,birthday,gender);
-		return "redirect:/login?success=true";
-	}
+    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+    public String addContact(@ModelAttribute("user") User user,
+                             BindingResult bindingResult) {
+        String birthday = (String) bindingResult.getFieldValue("birthday");
+        String gender = (String) bindingResult.getFieldValue("gender");
+        UserState userState = UserState.INACTIVE;
+        registrationService.register(user, birthday, gender, userState);
+        Thread mailSenderThread = new Thread(new Runnable() {
+            public void run() {
+                registrationSendMail.sendWelcomeMail(user);
+            }
+        });
+        mailSenderThread.start();
+        return "redirect:/login?registration=true";
+    }
+
+    @RequestMapping(value = "registration-confirm", method = RequestMethod.GET)
+    public String confirmRegistration(@RequestParam(value = "id", required = true) Integer userId) {
+        registrationService.activateAccount(userId);
+        return "redirect:/login?success=true";
+    }
 }
