386ee16334fc0a276e33e6a9510a41c2d3429656
louriel <viktoriiaromaniv@gmail.com>
2014-09-17 02:39:30 +0300

    Update SystemPropertiesService.java

--- src/main/java/com/softserve/edu/service/SystemPropertiesService.java
+++ src/main/java/com/softserve/edu/service/SystemPropertiesService.java
@@ -3,8 +3,8 @@ package com.softserve.edu.service;
 public interface SystemPropertiesService {
 	public static final String IMAGE_PATH_PROP = "IMAGE_PATH";
 	public static final String IMAGE_URL_PROP = "IMAGE_URL";
-	public static final String PASSWORD = "password";
-    	public static final String USERNAME = "username";
+	public static final String EMAIL_PASS_SEND = "password";
+    	public static final String EMAIL_LOGIN = "username";
     	public static final String BASE_SEND_URL = "url";
 	String getImagePath();
 	String getImageUrl();
