be9bd193e08c6a0d37cc4ef0589d364a04c67297
louriel <viktoriiaromaniv@gmail.com>
2014-09-17 02:32:31 +0300

    Now url is getting from DB

--- src/main/java/com/softserve/edu/service/SystemPropertiesService.java
+++ src/main/java/com/softserve/edu/service/SystemPropertiesService.java
@@ -5,8 +5,10 @@ public interface SystemPropertiesService {
 	public static final String IMAGE_URL_PROP = "IMAGE_URL";
 	public static final String PASSWORD = "password";
     	public static final String USERNAME = "username";
+    	public static final String BASE_SEND_URL = "url";
 	String getImagePath();
 	String getImageUrl();
 	public String getMailPass();
     	public String getMailUsername();
+    	public String getBaseUrl();
 }
